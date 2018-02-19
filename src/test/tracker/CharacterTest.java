424d8a89396e5b333962e76c3ec71e1af4544fd8
Sindre S. Kjær <sindre@sindrekjr.no>
2018-02-19 12:57:21 +0100

    Refactor jUnit to 5.0.0

--- src/test/tracker/CharacterTest.java
+++ src/test/tracker/CharacterTest.java
@@ -1,54 +1,54 @@
 package tracker;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
-public class CharacterTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTest {
 	
 	private final static String testName = "TestCharacter";
 	private final static int avgInitiative = 10;
 	private final static int avgDexterity = 10;
 	private Character c;
 	
-	@Before
-	public void setup() {
+	@BeforeEach
+	void setup() {
 		c = new Character(testName);
 	}
 	
 	@Test
-	public void setInitiative_assertThatSetterChangesInitiativeField() {
+	void setInitiative_assertThatSetterChangesInitiativeField() {
 		c.setInitiative(avgInitiative);
-		Assert.assertEquals(avgInitiative, c.getInitiative());
+		assertEquals(avgInitiative, c.getInitiative());
 	}
 	
 	@Test
-	public void setDexterity_assertThatSetterChangesDexterityField() {
+	void setDexterity_assertThatSetterChangesDexterityField() {
 		c.setDexterity(avgDexterity);
-		Assert.assertEquals(avgDexterity, c.getDexterity());
-	}
-
-	@Test(expected = EqualityException.class)
-	public void compareTo_throwsExceptionWhenComparingIdenticalCharacters() {
-		c.compareTo(c);
-	}
-
-	@Test(expected = EqualityException.class)
-	public void compareTo_throwsExceptionWhenComparingCharactersOfEqualInitiativeAndDexterity() {
-		c.setInitiative(avgInitiative);
-		c.setDexterity(avgDexterity);
-		c.compareTo(new Character("Guy", avgInitiative, avgDexterity));
+		assertEquals(avgDexterity, c.getDexterity());
 	}
 
 	@Test
-	public void verifyThatReadyFieldCanBeChanged() {
+	void compareTo_throwsExceptionWhenComparingIdenticalCharacters() {
+		assertThrows(EqualityException.class, () -> c.compareTo(c));
+	}
+
+	@Test
+	void compareTo_throwsExceptionWhenComparingCharactersOfEqualInitiativeAndDexterity() {
+		c.setInitiative(avgInitiative);
+		c.setDexterity(avgDexterity);
+		assertThrows(EqualityException.class, () -> c.compareTo(new Character("Guy", avgInitiative, avgDexterity)));
+	}
+
+	@Test
+	void verifyThatReadyFieldCanBeChanged() {
 		assert(!c.ready);
 		c.ready = true;
 		assert(c.ready);
 	}
 
 	@Test
-	public void verifyThatDelayFieldCanBeChanged() {
+	void verifyThatDelayFieldCanBeChanged() {
 		assert(!c.delay);
 		c.delay = true;
 		assert(c.delay);
--- src/test/tracker/TrackerTest.java
+++ src/test/tracker/TrackerTest.java
@@ -1,43 +1,43 @@
 package tracker;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
-public class TrackerTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackerTest {
 	
 	private final static Character character = new Character("Draugar");
 	private final static int num = 12;
 	private InitiativeTracker init;
 	
-	@Before
-	public void setup() {
+	@BeforeEach
+	void setup() {
 		init = new InitiativeTracker();
 	}
 	
 	@Test
-	public void add_addSingleCharacterToTracker() {
+	void add_addSingleCharacterToTracker() {
 		init.add(character);
-		Assert.assertEquals(character, init.getInitialOrder()[0]);
+		assertEquals(character, init.getInitialOrder()[0]);
 	}
 	
 	@Test
-	public void add_addSeveralCharactersToTracker() {
+	void add_addSeveralCharactersToTracker() {
 		for(int i = 1; i <= num; i++) init.add(new Character("Character" + i, i));
 	}
 	
 	@Test
-	public void add_checkThatCharactersAreSortedByInitiative() {
+	void add_checkThatCharactersAreSortedByInitiative() {
 		for(int i = 0; i <= num; i++) init.add(new Character("Character" + i, i));
 		
 		int initiative = num;
 		for(int i = 0; i <= num; i++) {
-			Assert.assertEquals(initiative--, init.getInitialOrder()[i].getInitiative());
+			assertEquals(initiative--, init.getInitialOrder()[i].getInitiative());
 		}
 	}
 	
 	@Test
-	public void add_addSeveralGroupsOfCharactersWhereAllInEachGroupHaveEqualInitiativeAndDexterity() {
+	void add_addSeveralGroupsOfCharactersWhereAllInEachGroupHaveEqualInitiativeAndDexterity() {
 		int initiative = num;
 		for(int i = 1; i <= 5; i++) {
 			init.add(new Character("Mob" + i, initiative, num), i);
@@ -46,47 +46,47 @@ public class TrackerTest {
 	}
 	
 	@Test
-	public void add_assertThatCharactersWithEqualInitiativeAreSortedByDexterity() {
+	void add_assertThatCharactersWithEqualInitiativeAreSortedByDexterity() {
 		Character highDex = new Character("First", 10, 20);
 		Character lowDex = new Character("Second", 10, 8);
 		
 		init.add(highDex);
 		init.add(lowDex);
-		Assert.assertEquals(highDex, init.getInitialOrder()[0]);
+		assertEquals(highDex, init.getInitialOrder()[0]);
 	}
 	
-	@Test(expected = EqualityException.class)
-	public void add_throwsExceptionWhenAttemptingToAddIdenticalCharacters() {
+	@Test
+	void add_throwsExceptionWhenAttemptingToAddIdenticalCharacters() {
 		init.add(character);
-		init.add(character);
+		assertThrows(EqualityException.class, () -> init.add(character));
 	}
 	
-	@Test(expected = EqualityException.class)
-	public void add_throwsExceptionWhenAttemptingToAddCharactersWithEqualInitiativeAndDexterity() {
+	@Test
+	void add_throwsExceptionWhenAttemptingToAddCharactersWithEqualInitiativeAndDexterity() {
 		init.add(new Character("Guy", num, num));
-		init.add(new Character("Gal", num, num));
+		assertThrows(EqualityException.class, () -> init.add(new Character("Gal", num, num)));
 	}
 	
 	@Test
-	public void size_assertSizeAfterAddingSeveralCharacters() {
+	void size_assertSizeAfterAddingSeveralCharacters() {
 		for(int i = 1; i <= num; i++) init.add(new Character("Character" + i, i));
-		Assert.assertEquals(num, init.size());
+		assertEquals(num, init.size());
 	}
 	
 	@Test
-	public void round_assertThatRoundMethodIncrementsRoundCounter() {
+	void round_assertThatRoundMethodIncrementsRoundCounter() {
 		for(int i = 1; i <= num; i++) {
 			init.round();
-			Assert.assertEquals(i, init.getRound());
+			assertEquals(i, init.getRound());
 		}
 	}
 	
 	@Test
-	public void getCurrentOrder_assertThatCurrentOrderIsEqualToInitialOrderWhenNoSpecialActionsHaveBeenDone() {
+	void getCurrentOrder_assertThatCurrentOrderIsEqualToInitialOrderWhenNoSpecialActionsHaveBeenDone() {
 		for(int i = 0; i <= num; i++) init.add(new Character("Character" + i, i));
 		
 		init.round();
-		Assert.assertArrayEquals(init.getInitialOrder(), init.getCurrentOrder());
+		assertArrayEquals(init.getInitialOrder(), init.getCurrentOrder());
 	}
 	
 	/*@Test
