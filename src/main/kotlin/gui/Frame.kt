8e1e91e3fc481ebcf2f7f24f2ccae421a730b7f8
Sindre S. Kjær <sindre@sindrekjr.no>
2018-02-24 18:13:57 +0100

    Created TrackerApplication for TornadoFX

---
+++ src/main/kotlin/gui/Frame.kt
@@ -0,0 +1,18 @@
+package gui
+
+import tornadofx.*
+
+class Frame : View("Initiative Tracker") {
+    override val root = borderpane {
+
+    }
+
+    /*init {
+        root += button {
+
+        }
+        root += label {
+
+        }
+    }*/
+}
